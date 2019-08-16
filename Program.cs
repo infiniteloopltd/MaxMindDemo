d69a10a2d4a6953884b2ff0f0f8aac2dd825dcba
Fiach Reid <fiach.reid@gmail.com>
2019-08-16 13:22:03 +0100

    Demo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MaxMind.GeoIP2;
+
+namespace ConsoleApplication27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // install-package MaxMind.GeoIP2
+            var reader = new DatabaseReader("GeoLite2-Country.mmdb");
+            var response = reader.Country("8.8.8.8");
+            Console.WriteLine(response.Country.IsoCode);
+        }
+    }
+}
