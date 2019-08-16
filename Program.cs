a62d7bf3d9a2197b8b9bbfbbcd4e15abdbd2734e
Fiach Reid <fiach.reid@gmail.com>
2019-08-16 15:23:57 +0100

    Added auto downloader

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
 using MaxMind.GeoIP2;
 
 namespace ConsoleApplication27
@@ -7,10 +11,25 @@ namespace ConsoleApplication27
     {
         static void Main(string[] args)
         {
-            // install-package MaxMind.GeoIP2
+            var fi = new FileInfo("GeoLite2-Country.mmdb");
+            if (!fi.Exists || (DateTime.Now - fi.LastWriteTime).TotalDays > 30)
+            {
+                DownloadGeoliteDB();
+            }
             var reader = new DatabaseReader("GeoLite2-Country.mmdb");
             var response = reader.Country("8.8.8.8");
             Console.WriteLine(response.Country.IsoCode);
         }
+
+        private static void DownloadGeoliteDB()
+        {
+            var wc = new WebClient();
+            var bData = wc.DownloadData("https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz");
+            var zippedStream = new MemoryStream(bData);
+            var files = Tar.ExtractTarGzEntries(zippedStream);
+            var db = files.First(zippedFile => zippedFile.FileName.EndsWith("mmdb"));
+            File.WriteAllBytes("GeoLite2-Country.mmdb", db.Contents);
+        }
+
     }
 }
---
+++ TarGz.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace ConsoleApplication27
+{
+    public class Tar
+    {
+        public static List<TarEntry> ExtractTarGzEntries(Stream stream)
+        {
+            var gzip = new GZipStream(stream, CompressionMode.Decompress);
+            var mTar = new MemoryStream();
+            gzip.CopyTo(mTar);
+            mTar.Seek(0, SeekOrigin.Begin);
+            return ExtractTarEntries(mTar);
+        }
+
+        private static List<TarEntry> ExtractTarEntries(Stream stream)
+        {
+            var lTarEntries = new List<TarEntry>();
+            var buffer = new byte[100];
+            while (true)
+            {
+                stream.Read(buffer, 0, 100);
+                var name = Encoding.ASCII.GetString(buffer).Trim('\0');
+                if (String.IsNullOrWhiteSpace(name))
+                    break;
+                stream.Seek(24, SeekOrigin.Current);
+                stream.Read(buffer, 0, 12);
+                var size = Convert.ToInt64(Encoding.ASCII.GetString(buffer, 0, 12).Trim('\0'), 8);
+                stream.Seek(376L, SeekOrigin.Current);
+                var buf = new byte[size];
+                stream.Read(buf, 0, buf.Length);
+                lTarEntries.Add(new TarEntry
+                {
+                    Contents = buf,
+                    FileName = name
+                });
+
+                var pos = stream.Position;
+
+                var offset = 512 - (pos % 512);
+                if (offset == 512)
+                    offset = 0;
+
+                stream.Seek(offset, SeekOrigin.Current);
+            }
+            return lTarEntries;
+        }
+
+        public class TarEntry
+        {
+            public string FileName { get; set; }
+            public byte[] Contents { get; set; }
+        }
+    }
+}
